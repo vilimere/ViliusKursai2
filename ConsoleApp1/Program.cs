5c1a738ad3127a052aa99c6e6ac3258566459c68
vilimere <vmeras@gmail.com>
2020-03-16 20:50:50 +0200

    Pirma Funkcija, max sk

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,38 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello and World!");
+
+            Console.Write("Iveskite pirma skaicius: ");
+            int sk1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Iveskite antra skaicius: ");
+            int sk2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Iveskite trecia skaicius: ");
+            int sk3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Is Jusu ivestu skaiciu didziausias yra: " + Max(sk1, sk2, sk3));
+
+        }
+        static int Max(int sk1, int sk2, int sk3)
+        {
+            if (sk1 > sk2 && sk1 > sk3)
+            {
+                return sk1;
+            }
+            else if (sk2 > sk1 && sk2 > sk3)
+            {
+                return sk2;
+            }
+            else if (sk3 > sk1 && sk3 > sk1)
+            {
+                return sk3;
+            }
+            else
+
+            { return 999999; }
         }
     }
 }
